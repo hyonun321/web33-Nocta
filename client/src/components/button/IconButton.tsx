d333e2cebcdfc23e302577f9860f9cbad670cb55
pipisebastian <yrt7998@pusan.ac.kr>
2024-11-11 06:56:40 +0900

    refactor: icon button 추가할 기능 주석 추가
    #80

--- client/src/components/button/IconButton.tsx
+++ client/src/components/button/IconButton.tsx
@@ -7,6 +7,7 @@ interface IconButtonProps {
 }
 
 export const IconButton = ({ icon, size, onClick }: IconButtonProps) => {
+  // TODO 추후 svg 파일을 받아올 수 있도록 수정 (사이드바 - 페이지 추가 버튼)
   return (
     <button className={iconButtonContainer({ size })} onClick={onClick}>
       <span className={iconBox({ size })}>{icon}</span>
