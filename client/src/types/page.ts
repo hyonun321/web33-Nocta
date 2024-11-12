93a0e4bbd166d2fd2796a08a08b5d0fa7a7c67fb
pipisebastian <yrt7998@pusan.ac.kr>
2024-11-13 00:09:43 +0900

    feat: postion, size 타입 추가
    #96

--- client/src/types/page.ts
+++ client/src/types/page.ts
@@ -8,3 +8,13 @@ export interface Page {
   isActive: boolean;
   isVisible: boolean;
 }
+
+export interface Position {
+  x: number;
+  y: number;
+}
+
+export interface Size {
+  width: number;
+  height: number;
+}
