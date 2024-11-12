c06831fe40158756fa33897d13fa3b0d59cd0ba1
pipisebastian <yrt7998@pusan.ac.kr>
2024-11-13 00:15:04 +0900

    feat: page 컴포넌트에 usePage 훅 적용
    #96

--- client/src/features/page/Page.tsx
+++ client/src/features/page/Page.tsx
@@ -1,33 +1,62 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useRef } from "react";
 import { Editor } from "@features/editor/Editor";
-import { pageContainer, pageHeader, pageTitle } from "./Page.style";
+import { Page as PageType } from "src/types/page";
+import { pageAnimation, resizeHandleAnimation } from "./Page.animation";
+import { pageContainer, pageHeader, pageTitle, resizeHandle } from "./Page.style";
 import { PageControlButton } from "./components/PageControlButton";
+import { usePage } from "./hooks/usePage";
 
-interface PageProps {
-  x?: number;
-  y?: number;
+interface PageProps extends PageType {
+  handlePageSelect: (pageId: number) => void;
+  handlePageClose: (pageId: number) => void;
 }
 
-export const Page = ({ x = 0, y = 0 }: PageProps) => {
-  const position = {
-    left: `${x}px`,
-    top: `${y}px`,
-  };
-
-  const handlePageMinimize = () => {};
-  const handlePageMaximize = () => {};
-  const handlePageClose = () => {};
+export const Page = ({
+  id,
+  x,
+  y,
+  zIndex,
+  isActive,
+  handlePageSelect,
+  handlePageClose,
+}: PageProps) => {
+  const pageRef = useRef<HTMLDivElement>(null);
+  const { position, size, pageDrag, pageResize, pageMinimize, pageMaximize } = usePage({ x, y });
 
   return (
-    <div className={pageContainer} style={position}>
-      <div className={pageHeader}>
-        <p className={pageTitle}>title</p>
-        <PageControlButton
-          onPageClose={handlePageClose}
-          onPageMaximize={handlePageMaximize}
-          onPageMinimize={handlePageMinimize}
+    <AnimatePresence>
+      <motion.div
+        ref={pageRef}
+        className={pageContainer}
+        initial={pageAnimation.initial}
+        animate={pageAnimation.animate({
+          x: position.x,
+          y: position.y,
+          isActive,
+        })}
+        style={{
+          width: `${size.width}px`,
+          height: `${size.height}px`,
+          zIndex,
+        }}
+        onPointerDown={() => handlePageSelect(id)}
+      >
+        <div className={pageHeader} onPointerDown={pageDrag}>
+          <p className={pageTitle}>title</p>
+          <PageControlButton
+            onPageClose={() => handlePageClose(id)}
+            onPageMaximize={pageMaximize}
+            onPageMinimize={pageMinimize}
+          />
+        </div>
+        <Editor />
+        <motion.div
+          className={resizeHandle}
+          onMouseDown={pageResize}
+          whileHover={resizeHandleAnimation.whileHover}
         />
-      </div>
-      <Editor />
-    </div>
+      </motion.div>
+    </AnimatePresence>
   );
 };
