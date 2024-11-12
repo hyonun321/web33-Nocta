1e40a763840546c8fe646fe2b6815e2100a17c64
minjungw00 <minjungw00@naver.com>
2024-11-13 03:29:03 +0900

    test: 테스트용 인메모리 MongoDB 구축
    #097

---
+++ server/src/app.module.spec.ts
@@ -0,0 +1,32 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { Test, TestingModule } from '@nestjs/testing';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppModule } from './app.module';
+
+describe('MongoDB Connection', () => {
+  let mongoServer: MongoMemoryServer;
+
+  beforeAll(async () => {
+    mongoServer = await MongoMemoryServer.create({
+        instance: {
+          port: 27017,
+        },
+      });
+    const uri = mongoServer.getUri();
+
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        MongooseModule.forRoot(uri), // in-memory MongoDB URI
+        AppModule,
+      ],
+    }).compile();
+  });
+
+  afterAll(async () => {
+    if (mongoServer) await mongoServer.stop();
+  });
+
+  it('should connect to MongoDB successfully', () => {
+    expect(true).toBe(true); // 실제 테스트 로직 작성
+  });
+});
