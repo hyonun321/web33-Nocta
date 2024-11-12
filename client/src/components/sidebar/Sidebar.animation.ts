d1757fc7b38d779259e074ad1c8007b03ff1104a
pipisebastian <yrt7998@pusan.ac.kr>
2024-11-13 00:15:47 +0900

    feat: 사이드바 애니메이션 추가
    #96

---
+++ client/src/components/sidebar/Sidebar.animation.ts
@@ -0,0 +1,16 @@
+export const animation = {
+  initial: {
+    x: -100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+  },
+  transition: {
+    type: "spring",
+    stiffness: 300,
+    damping: 30,
+    duration: 0.5,
+  },
+};
--- client/src/components/sidebar/Sidebar.tsx
+++ client/src/components/sidebar/Sidebar.tsx
@@ -1,26 +1,40 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { IconButton } from "@components/button/IconButton";
+import { Page } from "src/types/page";
 import { MenuButton } from "./MenuButton";
 import { PageItem } from "./PageItem";
-import { sidebarContainer, navWrapper } from "./Sidebar.style";
+import { animation } from "./Sidebar.animation";
+import { sidebarContainer, navWrapper, plusIconBox } from "./Sidebar.style";
 
-interface NavItem {
-  id: number;
-  page: string;
-  icon: string;
-}
-
-export const Sidebar = () => {
-  const navItems: NavItem[] = [
-    { id: 1, page: "Page 1", icon: "🏠" },
-    { id: 2, page: "Page 2", icon: "🔍" },
-    { id: 3, page: "Page 3", icon: "📚" },
-  ];
-
+export const Sidebar = ({
+  pages,
+  handlePageAdd,
+  handlePageSelect,
+}: {
+  pages: Page[];
+  handlePageAdd: () => void;
+  handlePageSelect: (pageId: number, isSidebar: boolean) => void;
+}) => {
   return (
     <aside className={sidebarContainer}>
       <MenuButton />
-      <nav className={navWrapper}>
-        {navItems?.map((item) => <PageItem key={item.id} {...item} />)}
-      </nav>
+      <AnimatePresence>
+        <nav className={navWrapper}>
+          {pages?.map((item) => (
+            <motion.div
+              key={item.id}
+              initial={animation.initial}
+              animate={animation.animate}
+              transition={animation.transition}
+            >
+              <PageItem {...item} onClick={() => handlePageSelect(item.id, true)} />
+            </motion.div>
+          ))}
+        </nav>
+      </AnimatePresence>
+      <motion.div className={plusIconBox}>
+        <IconButton icon="➕" size="sm" onClick={handlePageAdd} />
+      </motion.div>
     </aside>
   );
 };
