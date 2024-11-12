1a8c3679e4b8967e60dc3c827ced5694f1f4ccc0
pipisebastian <yrt7998@pusan.ac.kr>
2024-11-13 00:13:43 +0900

    feat: page 관리하는 usePagesManage 훅 구현
    - page 정보 관리
    - 사이드바, 바텀 네비게이션, 페이지 클릭에 따른 정보 관리
    
    #96

---
+++ client/src/features/workSpace/hooks/usePagesManage.ts
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { Page } from "src/types/page";
+
+interface usePagesManageProps {
+  pages: Page[];
+  addPage: () => void;
+  selectPage: (pageId: number, isSidebar?: boolean) => void;
+  closePage: (pageId: number) => void;
+}
+
+const INIT_ICON = "📄";
+const PAGE_OFFSET = 60;
+
+export const usePagesManage = (): usePagesManageProps => {
+  const [pages, setPages] = useState<Page[]>([]);
+
+  const getZIndex = () => {
+    return Math.max(0, ...pages.map((page) => page.zIndex)) + 1;
+  };
+
+  const addPage = () => {
+    const newPageIndex = pages.length;
+
+    setPages((prevPages) => [
+      ...prevPages.map((page) => ({ ...page, isActive: false })),
+      {
+        id: newPageIndex,
+        title: `Page ${newPageIndex + 1}`,
+        icon: INIT_ICON,
+        x: PAGE_OFFSET * newPageIndex,
+        y: PAGE_OFFSET * newPageIndex,
+        zIndex: getZIndex(),
+        isActive: true,
+        isVisible: true,
+      },
+    ]);
+  };
+
+  const selectPage = (pageId: number, isSidebar: boolean = false) => {
+    setPages((prevPages) =>
+      prevPages.map((page) => ({
+        ...page,
+        isActive: page.id === pageId,
+        ...(page.id === pageId && {
+          zIndex: getZIndex(),
+          isVisible: isSidebar ? true : page.isVisible,
+        }),
+      })),
+    );
+  };
+
+  const closePage = (pageId: number) => {
+    setPages((prevPages) =>
+      prevPages.map((page) => (page.id === pageId ? { ...page, isVisible: false } : page)),
+    );
+  };
+
+  return {
+    pages,
+    addPage,
+    selectPage,
+    closePage,
+  };
+};
