6c8fa35a620f1c44b548c7434926fb1c816df090
Yeonkyu Min <hailid3111@gmail.com>
2024-11-12 22:32:20 +0900

    fix: 화살표 로직 일부 수정

--- client/src/hooks/useMarkdownGrammer/handlers/arrow.ts
+++ client/src/hooks/useMarkdownGrammer/handlers/arrow.ts
@@ -14,6 +14,72 @@ export const useArrowKeyHandler = ({
       if (!currentNode) return;
 
       let targetNode: EditorNode | null = null;
+      
+      switch (e.key) {
+        case "ArrowUp":
+          if (currentNode.parentNode?.type === "checkbox") {
+            if (currentNode.parentNode.prevNode) {
+              if (currentNode.parentNode.prevNode.type === "checkbox") {
+                targetNode = currentNode.parentNode.prevNode.firstChild;
+              } else {
+                targetNode = currentNode.parentNode.prevNode;
+              }
+            }
+          } else if (currentNode.prevNode?.type === "ul" || currentNode.prevNode?.type === "ol") {
+            targetNode = editorList.getLastChild(currentNode.prevNode);
+          } else if (currentNode.type === "li") {
+            if (currentNode.prevSibling?.id) {
+              targetNode = currentNode.prevSibling;
+            } else {
+              if (currentNode.parentNode?.prevNode) {
+                if (currentNode.parentNode.prevNode.type === "checkbox") {
+                  // 이전 블록이 체크박스인 경우 처리
+                  targetNode = currentNode.parentNode.prevNode.firstChild;
+                } else {
+                  targetNode = currentNode.parentNode.prevNode;
+                }
+              } else {
+                return;
+              }
+            }
+          } else {
+            targetNode = currentNode.prevNode;
+          }
+          break;
+      
+        case "ArrowDown":
+          if (currentNode.parentNode?.type === "checkbox") {
+            if (currentNode.parentNode.nextNode) {
+              if (currentNode.parentNode.nextNode.type === "checkbox") {
+                targetNode = currentNode.parentNode.nextNode.firstChild;
+              } else {
+                targetNode = currentNode.parentNode.nextNode;
+              }
+            }
+          } else if (currentNode.nextNode?.type === "ul" || currentNode.nextNode?.type === "ol") {
+            targetNode = currentNode.nextNode.firstChild;
+          } else if (currentNode.type === "li") {
+            if (currentNode.nextSibling?.id) {
+              targetNode = currentNode.nextSibling;
+            } else {
+              if (currentNode.parentNode?.nextNode) {
+                if (currentNode.parentNode.nextNode.type === "checkbox") {
+                  // 다음 블록이 체크박스인 경우 처리
+                  targetNode = currentNode.parentNode.nextNode.firstChild;
+                } else {
+                  targetNode = currentNode.parentNode.nextNode;
+                }
+              } else {
+                return;
+              }
+            }
+          } else {
+            targetNode = currentNode.nextNode;
+          }
+          break;
+      }
+      
+      /*
       switch (e.key) {
         case "ArrowUp":
           if (currentNode.parentNode?.type === "checkbox") {
@@ -47,6 +113,7 @@ export const useArrowKeyHandler = ({
                 targetNode = currentNode.parentNode.nextNode;
               }
             }
+            
           } else if (currentNode.nextNode?.type === "ul" || currentNode.nextNode?.type === "ol") {
             targetNode = currentNode.nextNode!.firstChild;
           } else if (currentNode.type === "li") {
@@ -62,7 +129,7 @@ export const useArrowKeyHandler = ({
           }
           break;
       }
-
+      */
       if (targetNode) {
         setEditorState((prev) => ({
           ...prev,
