7b75bb17fea915e6dc264fddec6d1034384bdf64
Yeonkyu Min <hailid3111@gmail.com>
2024-11-13 00:31:14 +0900

    refactor: 화살표 핸들러 로직 유틸리티 함수로 리팩토링

--- client/src/hooks/useMarkdownGrammer/handlers/arrow.ts
+++ client/src/hooks/useMarkdownGrammer/handlers/arrow.ts
@@ -1,6 +1,7 @@
 import { useCallback } from "react";
 import { EditorNode } from "../../../types/markdown";
 import { KeyHandlerProps } from "./handlerProps";
+import { handleKeyNavigation } from "@utils/blockNavigationUtils";
 
 export const useArrowKeyHandler = ({
   editorState,
@@ -14,122 +15,17 @@ export const useArrowKeyHandler = ({
       if (!currentNode) return;
 
       let targetNode: EditorNode | null = null;
-
       switch (e.key) {
         case "ArrowUp":
-          if (currentNode.parentNode?.type === "checkbox") {
-            if (currentNode.parentNode.prevNode) {
-              if (currentNode.parentNode.prevNode.type === "checkbox") {
-                targetNode = currentNode.parentNode.prevNode.firstChild;
-              } else {
-                targetNode = currentNode.parentNode.prevNode;
-              }
-            }
-          } else if (currentNode.prevNode?.type === "ul" || currentNode.prevNode?.type === "ol") {
-            targetNode = editorList.getLastChild(currentNode.prevNode);
-          } else if (currentNode.type === "li") {
-            if (currentNode.prevSibling?.id) {
-              targetNode = currentNode.prevSibling;
-            } else {
-              if (currentNode.parentNode?.prevNode) {
-                if (currentNode.parentNode.prevNode.type === "checkbox") {
-                  // 이전 블록이 체크박스인 경우 처리
-                  targetNode = currentNode.parentNode.prevNode.firstChild;
-                } else {
-                  targetNode = currentNode.parentNode.prevNode;
-                }
-              } else {
-                return;
-              }
-            }
-          } else {
-            targetNode = currentNode.prevNode;
-          }
+        case "ArrowDown": {
+          e.preventDefault();
+          
+          const direction = e.key === "ArrowUp" ? "prev" : "next";
+          targetNode = handleKeyNavigation(currentNode, direction);
+          
           break;
-
-        case "ArrowDown":
-          if (currentNode.parentNode?.type === "checkbox") {
-            if (currentNode.parentNode.nextNode) {
-              if (currentNode.parentNode.nextNode.type === "checkbox") {
-                targetNode = currentNode.parentNode.nextNode.firstChild;
-              } else {
-                targetNode = currentNode.parentNode.nextNode;
-              }
-            }
-          } else if (currentNode.nextNode?.type === "ul" || currentNode.nextNode?.type === "ol") {
-            targetNode = currentNode.nextNode.firstChild;
-          } else if (currentNode.type === "li") {
-            if (currentNode.nextSibling?.id) {
-              targetNode = currentNode.nextSibling;
-            } else {
-              if (currentNode.parentNode?.nextNode) {
-                if (currentNode.parentNode.nextNode.type === "checkbox") {
-                  // 다음 블록이 체크박스인 경우 처리
-                  targetNode = currentNode.parentNode.nextNode.firstChild;
-                } else {
-                  targetNode = currentNode.parentNode.nextNode;
-                }
-              } else {
-                return;
-              }
-            }
-          } else {
-            targetNode = currentNode.nextNode;
-          }
-          break;
+        }
       }
-
-      /*
-      switch (e.key) {
-        case "ArrowUp":
-          if (currentNode.parentNode?.type === "checkbox") {
-            if (currentNode.parentNode.prevNode) {
-              if (currentNode.parentNode.prevNode.type === "checkbox") {
-                targetNode = currentNode.parentNode.prevNode.firstChild;
-              } else {
-                targetNode = currentNode.parentNode.prevNode;
-              }
-            }
-          } else if (currentNode.prevNode?.type === "ul" || currentNode.prevNode?.type === "ol") {
-            targetNode = editorList.getLastChild(currentNode.prevNode!);
-          } else if (currentNode.type === "li") {
-            if (currentNode.prevSibling?.id) {
-              targetNode = currentNode.prevSibling;
-            } else {
-              if (currentNode.parentNode?.prevNode) {
-                targetNode = currentNode.parentNode?.prevNode;
-              } else return;
-            }
-          } else {
-            targetNode = currentNode.prevNode;
-          }
-          break;
-        case "ArrowDown":
-          if (currentNode.parentNode?.type === "checkbox") {
-            if (currentNode.parentNode.nextNode) {
-              if (currentNode.parentNode.nextNode.type === "checkbox") {
-                targetNode = currentNode.parentNode.nextNode.firstChild;
-              } else {
-                targetNode = currentNode.parentNode.nextNode;
-              }
-            }
-            
-          } else if (currentNode.nextNode?.type === "ul" || currentNode.nextNode?.type === "ol") {
-            targetNode = currentNode.nextNode!.firstChild;
-          } else if (currentNode.type === "li") {
-            if (currentNode.nextSibling?.id) {
-              targetNode = currentNode.nextSibling;
-            } else {
-              if (currentNode.parentNode?.nextNode) {
-                targetNode = currentNode.parentNode?.nextNode;
-              } else return;
-            }
-          } else {
-            targetNode = currentNode.nextNode;
-          }
-          break;
-      }
-      */
       if (targetNode) {
         setEditorState((prev) => ({
           ...prev,
