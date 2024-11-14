7f917e5e122798f3d93b44a561bf2a575b1599a7
hyonun321 <hyo_nun@kakao.com>
2024-11-15 01:39:53 +0900

    refactor: 불필요한 interface 주석처리
    - 추후 사용가능성을 위해 주석처리
    
    #121

--- @noctaCrdt/Interfaces.ts
+++ @noctaCrdt/Interfaces.ts
@@ -1,21 +1,23 @@
-import { NodeId, Node } from "./Node";
+import { NodeId, BlockId, CharId } from "./NodeId";
+import { Block, Char } from "./Node";
 
 export type ElementType = "p" | "h1" | "h2" | "h3" | "ul" | "ol" | "li" | "checkbox" | "blockquote";
 
 export interface InsertOperation {
-  node: Node;
+  node: Block | Char;
 }
 
 export interface DeleteOperation {
-  targetId: NodeId | null;
+  targetId: BlockId | CharId;
   clock: number;
 }
+
 export interface RemoteInsertOperation {
-  node: Node;
+  node: Block | Char;
 }
 
 export interface RemoteDeleteOperation {
-  targetId: NodeId | null;
+  targetId: NodeId;
   clock: number;
 }
 
@@ -24,65 +26,53 @@ export interface CursorPosition {
   position: number;
 }
 
-export interface SerializedProps {
+export interface SerializedProps<T> {
+  // CRDT 직렬화라서 이름바꿔야함.
   clock: number;
   client: number;
-  textLinkedList: {
+  LinkedList: {
     head: NodeId | null;
-    nodeMap: { [key: string]: Node };
+    nodeMap: { [key: string]: T };
   };
 }
 
-export interface WorkSpace {
-  id: string;
-  pageList: Page[];
-  authUser: object;
-}
+// export interface WorkSpace {
+//   id: string;
+//   pageList: Page[];
+//   authUser: object;
+// }
 
-export interface Page {
-  id: string;
-  title: string;
-  icon: string; // 추후 수정
-  crdt: CRDT;
-}
+// export interface Page {
+//   id: string;
+//   title: string;
+//   icon: string; // 추후 수정
+//   crdt: EditorCRDT;
+// }
+// export interface LinkedList {
+//   head: NodeId | null;
+//   nodeMap: { [key: string]: Block | Char };
+// }
 
-export interface CRDT {
-  clock: number;
-  client: number;
-  LinkedList: LinkedList;
-  localInsert(index: number, value: string): RemoteInsertOperation;
-  localDelete(index: number): RemoteDeleteOperation;
-  remoteInsert(operation: RemoteInsertOperation): void;
-  remoteDelete(operation: RemoteDeleteOperation): void;
-  read(): string;
-  spread(): Block[] | Char[];
-}
+// export interface Block {
+//   id: BlockId;
+//   icon: string; // 추후 수정
+//   type: ElementType;
+//   animation: string;
+//   crdt: BlockCRDT;
+//   indent: number;
+//   next: NodeId;
+//   prev: NodeId;
+//   style: string[];
+// }
 
-export interface LinkedList {
-  head: NodeId | null;
-  nodeMap: { [key: string]: Block | Char };
-}
+// export interface Char {
+//   id: NodeId;
+//   value: string;
+//   next: NodeId | null;
+//   prev: NodeId | null;
+// }
 
-export interface Block {
-  id: BlockId;
-  icon: string; // 추후 수정
-  type: ElementType;
-  animation: string;
-  crdt: CRDT;
-  indent: number;
-  next: NodeId;
-  prev: NodeId;
-  style: string[];
-}
-
-export interface Char {
-  id: NodeId;
-  value: string;
-  next: NodeId | null;
-  prev: NodeId | null;
-}
-
-export interface BlockId {
-  clock: number;
-  client: number;
-}
+// export interface BlockId {
+//   clock: number;
+//   client: number;
+// }
