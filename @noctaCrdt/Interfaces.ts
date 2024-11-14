bfd3d3c43ad4dea2e95c53c0cc6a5b5130b058c5
hyonun321 <hyo_nun@kakao.com>
2024-11-14 17:59:47 +0900

    feat: WorkSpace, Page, ElementType 등의 interface 및 Type 선언
    #117

--- @noctaCrdt/Interfaces.ts
+++ @noctaCrdt/Interfaces.ts
@@ -1,5 +1,7 @@
 import { NodeId, Node } from "./Node";
 
+export type ElementType = "p" | "h1" | "h2" | "h3" | "ul" | "ol" | "li" | "checkbox" | "blockquote";
+
 export interface InsertOperation {
   node: Node;
 }
@@ -29,4 +31,58 @@ export interface SerializedProps {
     head: NodeId | null;
     nodeMap: { [key: string]: Node };
   };
+}
+
+export interface WorkSpace {
+  id: string;
+  pageList: Page[];
+  authUser: object;
+}
+
+export interface Page {
+  id: string;
+  title: string;
+  icon: string; // 추후 수정
+  crdt: CRDT;
+}
+
+export interface CRDT {
+  clock: number;
+  client: number;
+  LinkedList: LinkedList;
+  localInsert(index: number, value: string): RemoteInsertOperation;
+  localDelete(index: number): RemoteDeleteOperation;
+  remoteInsert(operation: RemoteInsertOperation): void;
+  remoteDelete(operation: RemoteDeleteOperation): void;
+  read(): string;
+  spread(): Block[] | Char[];
+}
+
+export interface LinkedList {
+  head: NodeId | null;
+  nodeMap: { [key: string]: Block | Char };
+}
+
+export interface Block {
+  id: BlockId;
+  icon: string; // 추후 수정
+  type: ElementType;
+  animation: string;
+  crdt: CRDT;
+  indent: number;
+  next: NodeId;
+  prev: NodeId;
+  style: string[];
+}
+
+export interface Char {
+  id: NodeId;
+  value: string;
+  next: NodeId | null;
+  prev: NodeId | null;
+}
+
+export interface BlockId {
+  clock: number;
+  client: number;
 }
