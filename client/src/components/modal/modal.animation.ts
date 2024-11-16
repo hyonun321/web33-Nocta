3d4ac437bcfa0b4ad7d9621c5c8b3cbea259aaa6
pipisebastian <yrt7998@pusan.ac.kr>
2024-11-16 21:47:22 +0900

    feat: 모달 애니메이션 추가
    #127

--- client/src/components/modal/modal.animation.ts
+++ client/src/components/modal/modal.animation.ts
@@ -11,19 +11,7 @@ export const overlayAnimation = {
 };
 
 export const modalContainerAnimation = {
-  initial: {
-    scale: 0.7,
-    opacity: 0,
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-  },
-  animate: {
-    scale: 1,
-    opacity: 1,
-  },
-  exit: {
-    scale: 0.7,
-    opacity: 0,
-  },
+  initial: { opacity: 0, y: 50, scale: 0.3 },
+  animate: { opacity: 1, y: 0, scale: 1 },
+  exit: { opacity: 0, scale: 0.5, transition: { duration: 0.2 } },
 };
