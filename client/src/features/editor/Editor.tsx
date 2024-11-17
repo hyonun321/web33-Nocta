c6e6a988f4b7904236e787073ca86d4f006753c5
Ludovico7 <hailid3111@gmail.com>
2024-11-17 18:03:07 +0900

    refactor: Block.tsx 상대경로로 수정....

--- client/src/features/editor/Editor.tsx
+++ client/src/features/editor/Editor.tsx
@@ -5,8 +5,8 @@ import { BlockLinkedList } from "@noctaCrdt/LinkedList";
 import { Block as CRDTBlock } from "@noctaCrdt/Node";
 import { BlockId } from "@noctaCrdt/NodeId";
 import { useRef, useState, useCallback, useEffect } from "react";
-import { Block } from "@features/editor/components/Block/Block";
 import { editorContainer, editorTitleContainer, editorTitle } from "./Editor.style";
+import { Block } from "./components/Block/Block";
 import { useBlockDragAndDrop } from "./hooks/useBlockDragAndDrop";
 import { useMarkdownGrammer } from "./hooks/useMarkdownGrammer";
 
