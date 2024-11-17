1963a0b5332d22887c60d64a97850f97298ce7d8
Ludovico7 <hailid3111@gmail.com>
2024-11-17 17:41:40 +0900

    style: lint 설정 -> 블록 컴포넌트, 키입력 커스텀 훅 디렉토리 구조 수정으로 상대 경로 import로 수정

--- client/src/features/editor/Editor.tsx
+++ client/src/features/editor/Editor.tsx
@@ -5,10 +5,10 @@ import { BlockLinkedList } from "@noctaCrdt/LinkedList";
 import { Block as CRDTBlock } from "@noctaCrdt/Node";
 import { BlockId } from "@noctaCrdt/NodeId";
 import { useRef, useState, useCallback, useEffect } from "react";
-import { Block } from "@src/features/editor/components/Block/Block";
-import { useMarkdownGrammer } from "@src/features/editor/hooks/useMarkdownGrammer";
 import { editorContainer, editorTitleContainer, editorTitle } from "./Editor.style";
+import { Block } from "./components/Block/Block";
 import { useBlockDragAndDrop } from "./hooks/useBlockDragAndDrop";
+import { useMarkdownGrammer } from "./hooks/useMarkdownGrammer";
 
 interface EditorProps {
   onTitleChange: (title: string) => void;
