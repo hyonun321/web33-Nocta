347aaf7b0012e394bd5d8c36bfb246b448f3b0fd
pipisebastian <yrt7998@pusan.ac.kr>
2024-11-17 20:04:37 +0900

    feat: 스크롤바 webkit 디자인 적용
    #127

--- client/src/styles/global.ts
+++ client/src/styles/global.ts
@@ -6,4 +6,21 @@ export const globalStyles = defineGlobalStyles({
     backgroundSize: "cover",
     // TODO 폰트 설정
   },
+
+  // 스크롤바 전체
+  "::-webkit-scrollbar": {
+    width: "8px",
+  },
+
+  // 스크롤바 전체 영역
+  "::-webkit-scrollbar-track": {
+    background: "transparent",
+    marginBottom: "12px",
+  },
+
+  // 스크롤바 핸들
+  "::-webkit-scrollbar-thumb": {
+    background: "white/50",
+    borderRadius: "lg",
+  },
 });
