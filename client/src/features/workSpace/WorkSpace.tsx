fca26c658979a65adc29a0e2e5ebf3a419215ac6
hyonun321 <hyo_nun@kakao.com>
2024-11-18 15:26:53 +0900

    refactor: ErrorModal 등록 및 introScreen 부분 수정
    #137

--- client/src/features/workSpace/WorkSpace.tsx
+++ client/src/features/workSpace/WorkSpace.tsx
@@ -1,25 +1,25 @@
 import { BottomNavigator } from "@components/bottomNavigator/BottomNavigator";
+import { ErrorModal } from "@components/modal/ErrorModal";
 import { Sidebar } from "@components/sidebar/Sidebar";
 import { Page } from "@features/page/Page";
 import { container, content } from "./WorkSpace.style";
-import { ErrorScreen } from "./components/ErrorScreen";
 import { IntroScreen } from "./components/IntroScreen";
 import { usePagesManage } from "./hooks/usePagesManage";
 import { useWorkspaceInit } from "./hooks/useWorkspaceInit";
 
 export const WorkSpace = () => {
-  const { isLoading, isInitialized, error } = useWorkspaceInit();
+  const { isLoading, isInitialized } = useWorkspaceInit();
 
   const { pages, addPage, selectPage, closePage, updatePageTitle } = usePagesManage();
   const visiblePages = pages.filter((page) => page.isVisible);
-
+  const error = true;
   if (error) {
-    return <ErrorScreen errorMessage={error.message} />;
+    return <ErrorModal errorMessage="test" />;
   }
 
   return (
     <>
-      <IntroScreen isVisible={isLoading} />
+      {isLoading} && <IntroScreen />
       <div
         className={container}
         style={{
