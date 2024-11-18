d266f2f0f95e5e72991442169c803d9d7eb95f4e
hyonun321 <hyo_nun@kakao.com>
2024-11-18 15:26:16 +0900

    refacotor: IntroScreen 내부 로직 변경

--- client/src/features/workSpace/components/IntroScreen.tsx
+++ client/src/features/workSpace/components/IntroScreen.tsx
@@ -1,17 +1,10 @@
 import { LoadingSpinner } from "@components/loading/LoadingSpinner";
 
-interface IntroScreenProps {
-  isVisible: boolean;
-}
-
-export const IntroScreen = ({ isVisible }: IntroScreenProps) => {
-  if (!isVisible) return null;
-
+export const IntroScreen = () => {
   return (
     <div
       className="fixed inset-0 flex items-center justify-center bg-white z-50"
       style={{
-        opacity: isVisible ? 1 : 0,
         transition: "opacity 0.5s ease-in-out",
       }}
     >
