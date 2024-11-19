943f086a307434c98c0af983bd869efc7ebf331a
pipisebastian <yrt7998@pusan.ac.kr>
2024-11-19 18:13:30 +0900

    feat: 토큰 갱신 api 추가
    #140

--- client/src/apis/auth.ts
+++ client/src/apis/auth.ts
@@ -1,9 +1,8 @@
 import { useMutation } from "@tanstack/react-query";
-
 import { useUserActions } from "@src/stores/useUserStore";
-import { fetch, unAuthorizationFetch } from "./axios";
+import { unAuthorizationFetch } from "./axios";
 
-export const useSignupMutation = () => {
+export const useSignupMutation = (onSuccess: () => void) => {
   const fetcher = ({
     name,
     email,
@@ -18,10 +17,13 @@ export const useSignupMutation = () => {
 
   return useMutation({
     mutationFn: fetcher,
+    onSuccess: () => {
+      onSuccess();
+    },
   });
 };
 
-export const useLoginMutation = () => {
+export const useLoginMutation = (onSuccess: () => void) => {
   const { setUserInfo } = useUserActions();
 
   const fetcher = ({ email, password }: { email: string; password: string }) => {
@@ -33,20 +35,44 @@ export const useLoginMutation = () => {
     onSuccess: (response) => {
       const { id, name, accessToken } = response.data;
       setUserInfo(id, name, accessToken);
+      onSuccess();
     },
   });
 };
 
-export const useLogoutMutation = () => {
+export const useLogoutMutation = (onSuccess: () => void) => {
   const { removeUserInfo } = useUserActions();
 
   const fetcher = () => {
-    return fetch.post("/auth/logout");
+    // TODO 추후 그냥 fetch로 변경 (access token 넣어줘야함)
+    return unAuthorizationFetch.post("/auth/logout");
   };
 
   return useMutation({
     mutationFn: fetcher,
     onSuccess: () => {
+      removeUserInfo();
+      onSuccess();
+    },
+  });
+};
+
+export const useRefreshMutation = () => {
+  const { updateAccessToken, removeUserInfo } = useUserActions();
+
+  const fetcher = () => {
+    // TODO 추후 그냥 fetch로 변경 (access token 넣어줘야함)
+    return unAuthorizationFetch.post("/auth/refresh");
+    // return fetch.post("/auth/refresh"); // refresh token은 알아서 보내질거임
+  };
+
+  return useMutation({
+    mutationFn: fetcher,
+    onSuccess: (response) => {
+      const { accessToken } = response.data;
+      updateAccessToken(accessToken);
+    },
+    onError: () => {
       removeUserInfo();
     },
   });
