38d5ac4f7da7c46717962e73702c8577a67e8bb8
pipisebastian <yrt7998@pusan.ac.kr>
2024-11-19 18:16:06 +0900

    feat: 새로고침할때마다 토큰받아오도록 수정
    #140

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import { WorkSpace } from "@features/workSpace/WorkSpace";
+import { useRefreshMutation } from "./apis/auth";
+import { useUserInfo } from "./stores/useUserStore";
 
 const App = () => {
   // TODO 라우터, react query 설정
+  const { id } = useUserInfo();
+  const { mutate: refreshToken } = useRefreshMutation();
+
+  useEffect(() => {
+    // 세션 스토리지에 정보가 있을 경우 토큰 갱신
+    if (id) refreshToken();
+  }, [id]);
 
   return (
     <>
