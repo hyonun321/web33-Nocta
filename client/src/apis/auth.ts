7cd64ae87251322f2d47c06a2e969fdbc571837f
pipisebastian <yrt7998@pusan.ac.kr>
2024-11-20 17:54:20 +0900

    fix: 새로고침할때 accesstoken이 업데이트 안되는 버그 해결
    - react query의 select를 잘못사용하고 있었음. onSuccess과정은 queryFn에 넣음
    
    #140

--- client/src/apis/auth.ts
+++ client/src/apis/auth.ts
@@ -3,17 +3,8 @@ import { useUserActions } from "@src/stores/useUserStore";
 import { unAuthorizationFetch, fetch } from "./axios";
 
 export const useSignupMutation = (onSuccess: () => void) => {
-  const fetcher = ({
-    name,
-    email,
-    password,
-  }: {
-    name: string;
-    email: string;
-    password: string;
-  }) => {
-    return unAuthorizationFetch.post("/auth/register", { name, email, password });
-  };
+  const fetcher = ({ name, email, password }: { name: string; email: string; password: string }) =>
+    unAuthorizationFetch.post("/auth/register", { name, email, password });
 
   return useMutation({
     mutationFn: fetcher,
@@ -26,15 +17,13 @@ export const useSignupMutation = (onSuccess: () => void) => {
 export const useLoginMutation = (onSuccess: () => void) => {
   const { setUserInfo } = useUserActions();
 
-  const fetcher = ({ email, password }: { email: string; password: string }) => {
-    return unAuthorizationFetch.post("/auth/login", { email, password });
-  };
+  const fetcher = ({ email, password }: { email: string; password: string }) =>
+    unAuthorizationFetch.post("/auth/login", { email, password });
 
   return useMutation({
     mutationFn: fetcher,
     onSuccess: (response) => {
       const { id, name, accessToken } = response.data;
-      console.log(accessToken);
       setUserInfo(id, name, accessToken);
       onSuccess();
     },
@@ -44,9 +33,7 @@ export const useLoginMutation = (onSuccess: () => void) => {
 export const useLogoutMutation = (onSuccess: () => void) => {
   const { removeUserInfo } = useUserActions();
 
-  const fetcher = () => {
-    return fetch.post("/auth/logout");
-  };
+  const fetcher = () => fetch.post("/auth/logout");
 
   return useMutation({
     mutationFn: fetcher,
@@ -60,15 +47,16 @@ export const useLogoutMutation = (onSuccess: () => void) => {
 export const useRefreshQuery = () => {
   const { updateAccessToken } = useUserActions();
 
+  const fetcher = () => fetch.get("/auth/refresh");
+
   return useQuery({
     queryKey: ["refresh"],
-    queryFn: () => fetch.get("/auth/refresh"),
-    enabled: false,
-    select: (response) => {
+    queryFn: async () => {
+      const response = await fetcher();
       const { accessToken } = response.data;
-      console.log(accessToken);
       updateAccessToken(accessToken);
       return response.data;
     },
+    enabled: false,
   });
 };
