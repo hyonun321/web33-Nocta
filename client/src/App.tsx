05970d9ce72e7bb89c26dc0c534079d11b15f184
pipisebastian <yrt7998@pusan.ac.kr>
2024-11-20 22:33:58 +0900

    feat: error 상태관리 store 추가
    #140

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
 import { useEffect } from "react";
 import { WorkSpace } from "@features/workSpace/WorkSpace";
 import { useRefreshQuery } from "./apis/auth";
+import { ErrorModal } from "./components/modal/ErrorModal";
+import { useErrorStore } from "./stores/useErrorStore";
 import { useUserInfo } from "./stores/useUserStore";
 
 const App = () => {
   const { id, name, accessToken } = useUserInfo();
   const { refetch: refreshToken } = useRefreshQuery();
+  const { isErrorModalOpen, errorMessage } = useErrorStore();
 
   useEffect(() => {
     if (id && name && !accessToken) {
@@ -15,6 +18,7 @@ const App = () => {
 
   return (
     <>
+      {isErrorModalOpen && <ErrorModal errorMessage={errorMessage} />}
       <WorkSpace />
     </>
   );
