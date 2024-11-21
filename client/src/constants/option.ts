f62153fc9d64ab351dc2ac3fa87544d98beda34c
pipisebastian <yrt7998@pusan.ac.kr>
2024-11-22 03:52:02 +0900

    feat: 옵션 상수 수정
    #162

--- client/src/constants/option.ts
+++ client/src/constants/option.ts
@@ -1,16 +1,39 @@
-export const ANIMATION_OPTIONS = [
-  { id: "none", label: "없음" },
-  { id: "highlight", label: "하이라이트" },
-  { id: "gradation", label: "그라데이션" },
-];
+import { AnimationType, ElementType } from "@noctaCrdt/Interfaces";
 
-export const TYPE_OPTIONS = [
-  { id: "p", label: "p" },
-  { id: "h1", label: "h1" },
-  { id: "h2", label: "h2" },
-  { id: "h3", label: "h3" },
-  { id: "ul", label: "ul" },
-  { id: "ol", label: "ol" },
-  { id: "checkbox", label: "checkbox" },
-  { id: "blockquote", label: "blockquote" },
-];
+export const OPTION_CATEGORIES = {
+  TYPE: {
+    id: "type",
+    label: "전환",
+    options: [
+      { id: "p", label: "p" },
+      { id: "h1", label: "h1" },
+      { id: "h2", label: "h2" },
+      { id: "h3", label: "h3" },
+      { id: "ul", label: "ul" },
+      { id: "ol", label: "ol" },
+      { id: "checkbox", label: "checkbox" },
+      { id: "blockquote", label: "blockquote" },
+    ] as { id: ElementType; label: string }[],
+  },
+  ANIMATION: {
+    id: "animation",
+    label: "애니메이션",
+    options: [
+      { id: "none", label: "없음" },
+      { id: "highlight", label: "하이라이트" },
+      { id: "gradation", label: "그라데이션" },
+    ] as { id: AnimationType; label: string }[],
+  },
+  DUPLICATE: {
+    id: "duplicate",
+    label: "복제",
+    options: null,
+  },
+  DELETE: {
+    id: "delete",
+    label: "삭제",
+    options: null,
+  },
+};
+
+export type OptionCategory = keyof typeof OPTION_CATEGORIES;
