c1c811172ba138be9cb53a83f66eb29e25dbbde6
pipisebastian <yrt7998@pusan.ac.kr>
2024-11-22 00:31:02 +0900

    feat: 옵션창 디자인 추가
    #162

---
+++ client/src/features/editor/components/OptionModal/OptionModal.style.ts
@@ -0,0 +1,22 @@
+import { css } from "@styled-system/css";
+
+export const optionModal = css({
+  zIndex: "10000",
+  position: "fixed",
+  borderRadius: "8px",
+  width: "160px",
+  padding: "8px",
+  background: "white",
+  boxShadow: "md",
+});
+
+export const optionButton = css({
+  borderRadius: "8px",
+  width: "100%",
+  paddingBlock: "4px",
+  paddingInline: "8px",
+  textAlign: "left",
+  _hover: {
+    backgroundColor: "gray.100/40",
+  },
+});
