f7ebfd58aaa69dbee60fb25314a68e337f3c88bf
pipisebastian <yrt7998@pusan.ac.kr>
2024-11-22 00:08:03 +0900

    feat: 옵션창 상수 추가
    #162

---
+++ client/src/constants/option.ts
@@ -0,0 +1,16 @@
+export const ANIMATION_OPTIONS = [
+  { id: "none", label: "없음" },
+  { id: "highlight", label: "하이라이트" },
+  { id: "gradation", label: "그라데이션" },
+];
+
+export const TYPE_OPTIONS = [
+  { id: "p", label: "p" },
+  { id: "h1", label: "h1" },
+  { id: "h2", label: "h2" },
+  { id: "h3", label: "h3" },
+  { id: "ul", label: "ul" },
+  { id: "ol", label: "ol" },
+  { id: "checkbox", label: "checkbox" },
+  { id: "blockquote", label: "blockquote" },
+];
