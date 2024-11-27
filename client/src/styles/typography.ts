a9dd5f596616097e8a145017a8d3483c711a2280
hyonun321 <hyo_nun@kakao.com>
2024-11-27 22:04:49 +0900

    feat: pretendard 폰트 추가
    - Bold, Medium 각 woff, woff2 개 추가.
    - 추후 스타일 지정때 사용 가능
    - 현재는 글로벌과 각 typography에만 반영. blod도 weight로 관리중
    
    #224

--- client/src/styles/typography.ts
+++ client/src/styles/typography.ts
@@ -3,7 +3,7 @@ import { defineTextStyles } from "@pandacss/dev";
 export const textStyles = defineTextStyles({
   "display-medium16": {
     value: {
-      fontFamily: "Inter",
+      fontFamily: "pretendard",
       fontWeight: "500",
       fontSize: "16px",
       lineHeight: "24px",
@@ -15,7 +15,7 @@ export const textStyles = defineTextStyles({
 
   "display-medium20": {
     value: {
-      fontFamily: "Inter",
+      fontFamily: "pretendard",
       fontWeight: "500",
       fontSize: "20px",
       lineHeight: "28px",
@@ -27,7 +27,7 @@ export const textStyles = defineTextStyles({
 
   "display-medium24": {
     value: {
-      fontFamily: "Inter",
+      fontFamily: "pretendard",
       fontWeight: "500",
       fontSize: "24px",
       lineHeight: "32px",
@@ -39,7 +39,7 @@ export const textStyles = defineTextStyles({
 
   "display-medium28": {
     value: {
-      fontFamily: "Inter",
+      fontFamily: "pretendard",
       fontWeight: "700",
       fontSize: "28px",
       lineHeight: "36px",
@@ -51,7 +51,7 @@ export const textStyles = defineTextStyles({
 
   "display-medium32": {
     value: {
-      fontFamily: "Inter",
+      fontFamily: "pretendard",
       fontWeight: "700",
       fontSize: "32px",
       lineHeight: "40px",
