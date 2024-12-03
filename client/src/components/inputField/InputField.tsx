95d1b14cd39e28241aa534118956e2fef250fa86
hyonun321 <hyo_nun@kakao.com>
2024-12-04 02:29:56 +0900

    style: inputField에 입력이 안된경우 border red로 변하게 수정
    #279

--- client/src/components/inputField/InputField.tsx
+++ client/src/components/inputField/InputField.tsx
@@ -7,11 +7,20 @@ interface InputFieldProps {
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   placeholder?: string;
   Icon?: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  isError?: boolean;
 }
 
-export const InputField = ({ type, name, value, onChange, placeholder, Icon }: InputFieldProps) => (
+export const InputField = ({
+  type,
+  name,
+  value,
+  onChange,
+  placeholder,
+  Icon,
+  isError,
+}: InputFieldProps) => (
   <div className={formGroup}>
-    <div className={inputContainer}>
+    <div className={`${inputContainer} ${isError ? "bd_1px_solid_red bdr_sm" : ""}`}>
       <input
         type={type}
         name={name}
@@ -22,6 +31,6 @@ export const InputField = ({ type, name, value, onChange, placeholder, Icon }: I
         required
       />
     </div>
-    {Icon && <Icon className={iconBox} />}
+    {Icon && <Icon className={`${iconBox} ${isError ? "c_red" : ""}`} />}
   </div>
 );
